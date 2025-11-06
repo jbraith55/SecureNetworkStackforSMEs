c77cd351753ead5d78cff5cab52c7ba61140f4f4
jbraith55 <j5braithwaite@outlook.com>
2025-11-06 22:49:25 +1100

    Update and rename 1a_packages.sh to Firewall_1_Packages.sh

--- Firewall_1_Packages.sh
+++ Firewall_1_Packages.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install -y nftables ipset iproute2 rsyslog vim
 sudo systemctl enable nftables rsyslog systemd-networkd
-echo "~ Packages installed and services enabled"
+echo "Packages installed and services enabled"
 
